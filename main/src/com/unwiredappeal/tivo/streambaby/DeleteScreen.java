7e2db76837db23b625f994d61d1a2dc3b4d763db
kearygriffin <kearygriffin@3f105aa8-e4d0-11dd-a893-8d7ce9fbef62>
2009-10-04 17:26:37 +0000

    Forgot to add DeleteScreen.java during last comit Added test class Mp4Test.java
    git-svn-id: http://streambaby.googlecode.com/svn/trunk@252 3f105aa8-e4d0-11dd-a893-8d7ce9fbef62

---
+++ main/src/com/unwiredappeal/tivo/streambaby/DeleteScreen.java
@@ -0,0 +1,103 @@
+package com.unwiredappeal.tivo.streambaby;
+
+import java.io.File;
+
+import com.tivo.hme.bananas.BApplicationPlus;
+import com.unwiredappeal.tivo.dir.DirEntry;
+import com.unwiredappeal.tivo.utils.Log;
+
+public class DeleteScreen extends ButtonScreen {
+
+	public String screenTitle = "Delete";
+	private DirEntry de;
+	
+
+	public DeleteScreen(BApplicationPlus app, DirEntry de) {
+		super(app);
+		this.de = de;
+	}
+	
+	@Override
+	public void render() {
+	}
+
+	@Override
+	protected int setupButtons(boolean isReturn) {
+		
+		addSimpleTextButton("Don't do anything", new ButtonHandler() { 
+			public boolean left() {
+				popBack();
+				return true;
+			}
+			public boolean right() {
+				return popBack();
+			}
+			public boolean select() {
+				return popBack();
+			}			
+		}, false);
+		
+		addSimpleTextButton("Permanently delete", new ButtonHandler() 
+		{ 
+				public boolean left() 
+				{
+					popBack();
+					return true;
+				}
+				public boolean right() 
+				{
+					SingleActionScreen.Action action = new SingleActionScreen.Action() 
+					{
+						public String go() 
+						{
+							File dFile = new File(de.uri);
+							if (dFile.delete()) 
+							{
+								Log.info("Delete succeeded: " + de.fileName);
+								return "File Deleted.";
+							} 
+							else 
+							{
+								Log.info("Delete Failed: " + de.fileName);				
+								return "Deletion Failed!";
+							}
+						}						
+					};					
+					popBack();
+					popBack();
+					getBApp().push(new SingleActionScreen(app, action, de.getStrippedFilename()), TRANSITION_LEFT);
+					return true;
+				}
+				public boolean select() {
+					SingleActionScreen.Action action = new SingleActionScreen.Action() 
+					{
+						public String go() 
+						{
+							File dFile = new File(de.uri);
+							if (dFile.delete()) 
+							{
+								Log.info("Delete succeeded: " + de.fileName);
+								return "File Deleted.";
+							} 
+							else 
+							{
+								Log.info("Delete Failed: " + de.fileName);				
+								return "Deletion Failed!";
+							}
+						}						
+					};					
+					popBack();
+					popBack();
+					getBApp().push(new SingleActionScreen(app, action, de.getStrippedFilename()), TRANSITION_LEFT);
+					return true;
+				}			
+			}, true);		
+			
+		return 0;
+	}
+
+	public String toString() {
+		return screenTitle;
+	}
+
+}
